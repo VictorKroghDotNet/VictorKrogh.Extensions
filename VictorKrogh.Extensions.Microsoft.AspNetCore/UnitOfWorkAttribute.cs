e0f91fcb8c79383ce910f423db7d0a3219f27e38
KingoBoiii <kingodk@outlook.dk>
2024-08-29 18:48:13 +0200

    Add project files.

---
+++ VictorKrogh.Extensions.Microsoft.AspNetCore/UnitOfWorkAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using VictorKrogh.Data;
+
+namespace Microsoft.AspNetCore;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public sealed class UnitOfWorkAttribute : Attribute
+{
+    private IUnitOfWork? UnitOfWork { get; set; }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        UnitOfWork = context.HttpContext.RequestServices.GetRequiredService<IUnitOfWork>();
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        if (UnitOfWork != null && context.Exception == null)
+        {
+            UnitOfWork.Commit();
+        }
+    }
+
+}
